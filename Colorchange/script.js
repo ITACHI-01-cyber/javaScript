920e0e35955f49e432e813769c38f3a7afd3437a
ITACHI-01-cyber <bvivek514@gmail.com>
2025-09-09 12:31:34 +0000

    project

--- Colorchange/script.js
+++ Colorchange/script.js
@@ -1,17 +1,4 @@
-# Project related to DOM
 
-## Project Link:
-[click here](https://stackblitz.com/edit/dom-project-chaiaurcode?file=index.html)
-
-## Source Code File
-[click here] ()
-
-# Solution code
-
-##Project 01
-
-```javascript
-    console.log("vivek")
    const buttons =  document.querySelectorAll('.butoon')
 //    console.log(buttons)
 const body = document.querySelector("body")
@@ -36,5 +23,3 @@ buttons.forEach(function(button){
 
     });
 })
-
-```
